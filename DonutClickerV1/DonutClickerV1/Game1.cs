712cb78eb4ff98ba94233b112c0cc958f80499d6
John Wills <jrwills@ksu.edu>
2021-11-06 20:09:05 -0500

    trying to add simple score for when the donut is clicked

--- DonutClickerV1/DonutClickerV1/Game1.cs
+++ DonutClickerV1/DonutClickerV1/Game1.cs
@@ -8,6 +8,11 @@ namespace DonutClickerV1
     {
 
         Texture2D donutTexture;
+        Vector2 mainDonutPos;
+        Vector2 scaling; //idk bout this one
+
+        private SpriteFont font; //For the "Score :"
+        private int clickScore = 0; //initially zero
 
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
@@ -22,6 +27,8 @@ namespace DonutClickerV1
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+            mainDonutPos = new Vector2(300, 160);
+            scaling = new Vector2(1f, 1f);
 
             base.Initialize();
         }
@@ -31,7 +38,8 @@ namespace DonutClickerV1
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            donutTexture = Content.Load<Texture2D>("DonutSprite");
+            donutTexture = Content.Load<Texture2D>("DonutSprite2"); //argh
+            font = Content.Load<SpriteFont>("Score"); //Score = name of file
         }
 
         protected override void Update(GameTime gameTime)
@@ -49,7 +57,8 @@ namespace DonutClickerV1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             _spriteBatch.Begin();
-            _spriteBatch.Draw(donutTexture, new Vector2(0, 0), Color.White);
+            _spriteBatch.Draw(donutTexture, mainDonutPos, null, Color.White, 0f, Vector2.Zero, scaling, SpriteEffects.None, 0f); //This sucks
+            _spriteBatch.DrawString(font, "a", new Vector2(0, 0), Color.Black);
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
