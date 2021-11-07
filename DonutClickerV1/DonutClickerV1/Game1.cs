ba0f352d9b463005b656523a1352fa443d5d4324
John Wills <jrwills@ksu.edu>
2021-11-06 22:01:23 -0500

    getting somewhere, added police texture

--- DonutClickerV1/DonutClickerV1/Game1.cs
+++ DonutClickerV1/DonutClickerV1/Game1.cs
@@ -9,12 +9,15 @@ namespace DonutClickerV1
 
         Texture2D donutTexture;
         Texture2D bankTexture;
+        Texture2D policeTexture;
 
         Vector2 mainDonutPos;
         Vector2 bankPos;
+        Vector2 policePos;
 
         Vector2 scalingDonut; //idk bout this one
         Vector2 scalingBank;
+        Vector2 scalingPolice;
 
         private SpriteFont font; //For the "Score :"
         private int clickScore = 0; //initially zero
@@ -38,6 +41,9 @@ namespace DonutClickerV1
             bankPos = new Vector2(450,100);
             scalingDonut = new Vector2(1f, 1f);
 
+            policePos = new Vector2(456, -5);
+            scalingPolice = new Vector2(1f, 1f);
+
             base.Initialize();
         }
 
@@ -48,11 +54,13 @@ namespace DonutClickerV1
             // TODO: use this.Content to load your game content here
             donutTexture = Content.Load<Texture2D>("DonutSprite2"); //argh
             bankTexture = Content.Load<Texture2D>("BankSprite");
+            policeTexture = Content.Load<Texture2D>("PoliceSprite");
             font = Content.Load<SpriteFont>("Score"); //Score = name of file
         }
 
         protected override void Update(GameTime gameTime)
         {
+            
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
@@ -65,6 +73,8 @@ namespace DonutClickerV1
             // Get the mouse state relevant for this frame
             MouseInput.MouseState = Mouse.GetState();
 
+            //if (clickScore == 10 && MouseInput.MouseState.LeftButton == ButtonState.Pressed)
+
             base.Update(gameTime);
         }
 
@@ -76,6 +86,7 @@ namespace DonutClickerV1
             _spriteBatch.Begin();
             _spriteBatch.Draw(donutTexture, mainDonutPos, null, Color.White, 0f, Vector2.Zero, scalingDonut, SpriteEffects.None, 0f); //This sucks
             _spriteBatch.Draw(bankTexture, bankPos, null, Color.White, 0f, Vector2.Zero, scalingBank, SpriteEffects.None, 0f); //This sucks too
+            _spriteBatch.Draw(policeTexture, policePos, null, Color.White, 0f, Vector2.Zero, scalingPolice, SpriteEffects.None, 0f); //This sucks too-er
             _spriteBatch.DrawString(font, "Score: " + clickScore, new Vector2(0, 0), Color.Black);
             _spriteBatch.End();
 
