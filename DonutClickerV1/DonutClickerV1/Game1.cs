b7837bbb07c91bcb6d05e244194df24e19dd3b30
John Wills <jrwills@ksu.edu>
2021-11-06 23:09:43 -0500

    idk

--- DonutClickerV1/DonutClickerV1/Game1.cs
+++ DonutClickerV1/DonutClickerV1/Game1.cs
@@ -6,6 +6,32 @@ namespace DonutClickerV1
 {
     public class Game1 : Game
     {
+        //http://www.alecjacobson.com/weblog/?p=539
+        enum ButtState
+        {
+            HOVER,
+            UP,
+            JUST_RELEASED,
+            DOWN
+        }
+
+        const int NUMBER_OF_BUTTONS = 3,
+            COP_BUTTON_INDEX = 0,
+            ALIEN_BUTTON_INDEX = 1,
+            BOSTON_BUTTON_INDEX = 2,
+            BUTTON_HEIGHT = 40,
+            BUTTON_WIDTH = 88;
+        Color background_color;
+        Color[] button_color = new Color[NUMBER_OF_BUTTONS];
+        Rectangle[] button_rectangle = new Rectangle[NUMBER_OF_BUTTONS];
+        ButtState[] button_state = new ButtState[NUMBER_OF_BUTTONS];
+        Texture2D[] button_texture = new Texture2D[NUMBER_OF_BUTTONS];
+        double[] button_timer = new double[NUMBER_OF_BUTTONS];
+        //mouse pressed and mouse just pressed
+        bool mpressed, prev_mpressed = false;
+        //mouse location in window
+        int mx, my;
+        double frame_time;
 
         Texture2D donutTexture;
         Texture2D bankTexture;
@@ -14,6 +40,7 @@ namespace DonutClickerV1
         Vector2 mainDonutPos;
         Vector2 bankPos;
         Vector2 policePos;
+        Vector2 policePos2;
 
         Vector2 scalingDonut; //idk bout this one
         Vector2 scalingBank;
@@ -45,7 +72,25 @@ namespace DonutClickerV1
             scalingDonut = new Vector2(1f, 1f);
 
             policePos = new Vector2(456, -5);
+            policePos2 = new Vector2(0, 200);
             scalingPolice = new Vector2(1f, 1f);
+
+            // starting x and y locations to stack buttons (see link above)
+            // vertically in the middle of the screen
+            int x = Window.ClientBounds.Width / 2 - BUTTON_WIDTH / 2;
+            int y = Window.ClientBounds.Height / 2 -
+                NUMBER_OF_BUTTONS / 2 * BUTTON_HEIGHT -
+                (NUMBER_OF_BUTTONS % 2) * BUTTON_HEIGHT / 2;
+            for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+            {
+                button_state[i] = ButtState.UP;
+                button_color[i] = Color.White;
+                button_timer[i] = 0.0;
+                button_rectangle[i] = new Rectangle(x, y, BUTTON_WIDTH, BUTTON_HEIGHT);
+                y += BUTTON_HEIGHT;
+            }
+            IsMouseVisible = true;
+            background_color = Color.CornflowerBlue;
 
             base.Initialize();
         }
@@ -58,8 +103,11 @@ namespace DonutClickerV1
             donutTexture = Content.Load<Texture2D>("DonutSprite2"); //argh
             bankTexture = Content.Load<Texture2D>("BankSprite");
             policeTexture = Content.Load<Texture2D>("PoliceSprite");
+            button_texture[COP_BUTTON_INDEX] = Content.Load<Texture2D>("CopButton");
+
             font = Content.Load<SpriteFont>("Score"); //Score = name of file
             purchases = Content.Load<SpriteFont>("Purchases");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -99,8 +147,16 @@ namespace DonutClickerV1
             _spriteBatch.Draw(donutTexture, mainDonutPos, null, Color.White, 0f, Vector2.Zero, scalingDonut, SpriteEffects.None, 0f); //This sucks
             _spriteBatch.Draw(bankTexture, bankPos, null, Color.White, 0f, Vector2.Zero, scalingBank, SpriteEffects.None, 0f); //This sucks too
             _spriteBatch.Draw(policeTexture, policePos, null, Color.White, 0f, Vector2.Zero, scalingPolice, SpriteEffects.None, 0f); //This sucks too-er
+
+           
+            if(clickScore == 10 && MouseInput.MouseState.X >= 450 && MouseInput.MouseState.X <= 500 && MouseInput.MouseState.Y <= 500)
+                _spriteBatch.Draw(policeTexture, policePos2, null, Color.White, 0f, Vector2.Zero, scalingPolice, SpriteEffects.None, 0f); //This sucks too-er-er
+
             _spriteBatch.DrawString(font, "Score: " + clickScore, new Vector2(0, 0), Color.Black);
             //_spriteBatch.DrawString(font, text, new Vector2(0, 400), Color.Black);
+            /*for (int i = 0; i < NUMBER_OF_BUTTONS; i++)
+                _spriteBatch.Draw(button_texture[i], button_rectangle[i], button_color[i]);*/
+            _spriteBatch.Draw(button_texture[COP_BUTTON_INDEX], new Vector2(550,-5), button_color[COP_BUTTON_INDEX]);
             _spriteBatch.End();
 
             // TODO: Add your drawing code here
